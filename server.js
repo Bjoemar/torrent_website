fbf9f053275be625fb0f3c7fb9e8e40b31cfce80
bjoemar <bacleaanjoemar22@gmail.com>
2020-03-14 13:14:16 +0800

    Change the database url

--- server.js
+++ server.js
@@ -157,19 +157,19 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
-var Movie = 0;
+// var Movie = 0;
 
 
-// server.listen(server_port , server_ip_address , function(){
-// 	console.log('Listening on' + server_ip_address + ', port' + server_port);	
-// })
-
-
-
-
-server.listen(5000,function(){
-	console.log('Starting server on port5000');
+server.listen(server_port , server_ip_address , function(){
+	console.log('Listening on' + server_ip_address + ', port' + server_port);	
 })
+
+
+
+
+// server.listen(5000,function(){
+// 	console.log('Starting server on port5000');
+// })
 
 
 
